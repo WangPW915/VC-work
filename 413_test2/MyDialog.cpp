ec40ecfd01e26431c33e2649f012f03170dfc14d
WangPW915 <1832738390@qq.com>
2020-04-13 23:53:10 +0800

    4月13日练习2：点击菜单，弹出对话框，在对话框上实现能做加法运算的计算器；

---
+++ 413_test2/MyDialog.cpp
@@ -0,0 +1,50 @@
+// MyDialog.cpp : 实现文件
+//
+
+#include "stdafx.h"
+#include "413_test2.h"
+#include "MyDialog.h"
+#include "afxdialogex.h"
+
+
+// MyDialog 对话框
+
+IMPLEMENT_DYNAMIC(MyDialog, CDialogEx)
+
+MyDialog::MyDialog(CWnd* pParent /*=NULL*/)
+	: CDialogEx(MyDialog::IDD, pParent)
+	, a(0)
+	, b(0)
+	, c(0)
+{
+
+}
+
+MyDialog::~MyDialog()
+{
+}
+
+void MyDialog::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+	DDX_Text(pDX, IDC_EDIT1, a);
+	DDX_Text(pDX, IDC_EDIT2, b);
+	DDX_Text(pDX, IDC_EDIT3, c);
+}
+
+
+BEGIN_MESSAGE_MAP(MyDialog, CDialogEx)
+	ON_BN_CLICKED(IDC_BUTTON1, &MyDialog::OnBnClickedButton1)
+END_MESSAGE_MAP()
+
+
+// MyDialog 消息处理程序
+
+
+void MyDialog::OnBnClickedButton1()
+{
+	// TODO:  在此添加控件通知处理程序代码
+	this->UpdateData(true);
+	c = a + b;
+	UpdateData(false);
+}
